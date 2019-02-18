0fe45eba1f6754755ff27dd08f489325fd8f9036
SiennaS <ssienna48@gmail.com>
2019-02-18 17:31:57 -0500

    server

--- server.js
+++ server.js
@@ -35,13 +35,6 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
-// // Hook our mongojs config to the dbAvar
-// var dbA= mongojs(databaseUrl, collections);
-// db.on("error", function(error) {
-//   console.log("Database Error:", error);
-// });
-
-
 // Simple index route
 app.get("/", function(req, res) {
   res.render("scraper")
@@ -69,7 +62,7 @@ app.get("/scrape", function(req, res) {
         .attr("href");
 
       // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
+      dbA.Article.create(result)
         .then(function(dbArticle) {
           // View the added result in the console
           console.log(dbArticle);
@@ -79,15 +72,13 @@ app.get("/scrape", function(req, res) {
           console.log(err);
         });
     });
-
-        // Send a message to the client
         res.send("Scrape Complete");
       });
     });
 
-// Route for getting all Articles from the db
+// Route for getting all Articles from the dbA
 app.get("/articles", function(req, res) {
-  db.Article.find({})
+  dbA.Article.find({})
     .then(function(dbArticle) {
       // If successful, send results back to the client
       res.json(dbArticle);
@@ -100,39 +91,96 @@ app.get("/articles", function(req, res) {
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne(
-    { _id: req.params.id }
-    )
-    .populate("note")
-    .then(function(dbArticle) {
+
+  var id = req.params.id
+  dbA.findOne({ _id: id })
+    .then(function (dbArticle) {
+
+      var saveArticle = {
+        title: dbArticle.title,
+        link: dbArticle.link,
+        description: dbArticle.description,
+        image: dbArticle.image
+      }
+      dbS.create(saveArticle)
       res.json(dbArticle);
     })
     .catch(function(err) {
       res.json(err);
     });
+  });
+
+// Route for deleting Saved Articles
+app.delete("/clear", function (req, res) {
+  dbA.deleteMany({}, function (err) { });
+})
+
+// Route for grabbing saved Articles
+app.get("savedArticles", function (req,res) {
+  dbS.find({})
+  .then(function (dbArticle) {
+    res.json(dbArticle);
+  })
+  .catch(function (err) {
+    res.json(err);
+  });
+});
+
+// Route for rendering Saved Articles
+app.get("/saved", function (req, res) {
+  res.render("saved")
+});
+
+// Route for deleting a specific saved article
+app.delete("/deletesaved/:id", function (req,res) {
+  var id = req.params.id
+  dbS.findByIdAndRemove(id, function () {
+  })
+  res.render("saved")
 });
 
 
-// save/update an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  db.Note.create(req.body)
-    .then(function(dbNote) {
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
-            // { new: true } tells the query that we want it to return the updated Article -- it returns the original by default
-    })
-    .then(function(dbArticle) {
+// Route for deleting all saved Articles
+app.delete("/clearsaved", function (req, res) {
+  dbS.deleteMany({}, function (err) { });
+})
+
+// Route for grabbing Article note
+app.get("./notes/:id", function (req,res) {
+  dbN.find({articleId: req.params.id})
+  .then(function (dbNote) {
+    res.json(dbNote)
+  })
+  .catch(function (err) {
+    res.json(err);
+  });
+});
+
+// Route for article Note and with no match exists, then update inserts
+app.post("/articles/:id", function (req,res) {
+  dbN.create(req.body)
+  .then(function (dbNote) {
+    return dbN.findOneAndModify({_id: req.params.id}, {note: dbNote._id}, {upsert: true}, {new: true});
+  })
+    .then(function (dbArticle) {
       res.json(dbArticle);
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.json(err);
     });
-});
+  });
 
 
+// Route for deleting specific note
+  app.delete("/deleteNote/:id", function (req,res) {
+    var id = req.params.id
+    dbN.findByIdAndRemove(id, function () {
+    })
+    res.render("saved")
+  })
 
 
 // Start the server
 app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+  console.log("App running on port " + PORT );
 });
